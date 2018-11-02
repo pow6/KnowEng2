47c1daf129944d0c8e5140af417974137f59789d
pow6 <fake.existences@gmail.com>
2018-11-02 09:49:40 +0900

    include bug

--- kadai2.c
+++ kadai2.c
@@ -3,44 +3,40 @@
 #include <stdlib.h>
 
 #define numOfFeature 196
+#define numOfData 180
 #define fileRead(fileName,fileStream) fileStream=fopen(fileName,"r");if(fileStream==NULL){printf("cannat read file[%s]",fileName);exit(1);}
 #define fileWrite(fileName,fileStream) fileStream=fopen(fileName,"w");if(fileStream==NULL){printf("cannat write file[%s]",fileName);exit(1);}
 
-void readData(int numOfData,double data[][numOfFeature],char fileName[]);
-void writeData(int numOfData,double average[],char fileName[]);
-double oneElement_calcFeature(double data[][numOfFeature],int numOfData,int target);
-void average_calcFeature(double data[][numOfFeature],double average[],int numOfData);
+void readData(double data[][numOfFeature],char fileName[]);
+void writeData(double average[],char fileName[]);
+void writeDataTwoDim(double coriance[][numOfFeature],char fileName[]);
+double oneElement_calcFeature(double data[][numOfFeature],int target);
+void average_calcFeature(double data[][numOfFeature],double average[]);
+void calcCovariance(double data[][numOfFeature],double average[],double covariance[][numOfFeature]);
 
 void main(){
-    char fnReadFormat[12],fnWriteFormat[12];
+    char fnReadFormat[12]="c",fnWriteFormat[12]="sigma";
     char fnRead[24],fnWrite[24];
-    int numOfData,i,j;
-    double (*data)[numOfFeature],average[numOfFeature];
-    printf("読み込むデータ数：\n");
-    scanf("%d",&numOfData);
-    printf("入力ファイルフォーマット（数字前の先頭の文字を入力/c01.txt~c46.txtなら、cを入力）：");
-    scanf("%s",fnReadFormat);
-    printf("出力ファイルフォーマット（数字前の先頭の文字を入力/mean01.txt~mean46.txtなら、meanを入力）：");
-    scanf("%s",fnWriteFormat);
+    int i,j;
+    double (*data)[numOfFeature],average[numOfFeature],(*covariance)[numOfFeature];
     data = malloc(sizeof(double)*numOfData*numOfFeature);
+    covariance = malloc(sizeof(double)*numOfData*numOfFeature);
     for(i=0;i<46;i++){
-        //sprintf(fnRead,"%s%02d.txt",fnReadFormat,i+1);
         sprintf(fnRead,"./originData/%s%02d.txt",fnReadFormat,i+1);
-        printf("Read[%s]\n",fnRead);
-        readData(numOfData,data,fnRead);
-        average_calcFeature(data,average,numOfData);
-        //sprintf(fnWrite,"%s%02d.txt",fnWriteFormat,i+1);
-        sprintf(fnWrite,"./meanData/%s%02d.txt",fnWriteFormat,i+1);
-        printf("Write[%s]\n",fnWrite);
-        writeData(numOfData,average,fnWrite);
+        readData(data,fnRead);
+        average_calcFeature(data,average);
+        calcCovariance(data,average,covariance);
+        sprintf(fnWrite,"./sigmaData/%s%02d.txt",fnWriteFormat,i+1);
+        writeDataTwoDim(covariance,fnWrite);
     }
 }
 
 //ファイルからデータを読み取る
-void readData(int numOfData,double data[][numOfFeature],char fileName[]){
+void readData(double data[][numOfFeature],char fileName[]){
     FILE *read;
     int i,j;
     fileRead(fileName,read);
+    printf("Read[%s]\n",fileName);
     for(j=0;j<numOfData;j++){
         for(i=0;i<numOfFeature;i++){
             fscanf(read,"%lf",&data[j][i]);
@@ -50,18 +46,34 @@ void readData(int numOfData,double data[][numOfFeature],char fileName[]){
 }
 
 //ファイルに配列を書き込む
-void writeData(int numOfData,double average[],char fileName[]){
+void writeData(double average[],char fileName[]){
     FILE *write;
     int i;
     fileWrite(fileName,write);
+    printf("Write[%s]\n",fileName);
     for(i=0;i<numOfData;i++){
         fprintf(write,"%lf\n",average[i]);
     }
     fclose(write);
 }
 
+//ファイルに２次元配列を書き込む
+void writeDataTwoDim(double coriance[][numOfFeature],char fileName[]){
+    FILE *write;
+    int i,j;
+    fileWrite(fileName,write);
+    printf("Write[%s]\n",fileName);
+    for(i=0;i<numOfData;i++){
+        for(j=0;j<numOfFeature;j++){
+            fprintf(write,"%lf ",coriance[i][j]);
+        }
+        fprintf(write,"\n");
+    }
+    fclose(write);
+}
+
 //１つの要素分の平均特徴量を計算
-double oneElement_calcFeature(double data[][numOfFeature],int numOfData,int target){
+double oneElement_calcFeature(double data[][numOfFeature],int target){
     int i;
     double result=0;
     for(i=0;i<numOfData;i++){
@@ -70,11 +82,28 @@ double oneElement_calcFeature(double data[][numOfFeature],int numOfData,int targ
     return result/numOfData;
 }
 
-//指定数の文字分の平均特徴量を計算
-void average_calcFeature(double data[][numOfFeature],double average[],int numOfData){
+//指定数の文字分の平均特徴量を計算(代入)
+void average_calcFeature(double data[][numOfFeature],double average[]){
     int i;
     for(i=0;i<numOfFeature;i++){
-        average[i]=oneElement_calcFeature(data,numOfData,i);
+        average[i]=oneElement_calcFeature(data,i);
+    }
+}
+
+//共分散を計算する（各要素ごとに計算）
+void calcCovariance(double data[][numOfFeature],double average[],double covariance[][numOfFeature]){
+    int i,j,z;
+    for(i=0;i<numOfData;i++){
+        for(j=0;j<numOfFeature;j++)
+            covariance[i][j]=0;
+    }
+    for(i=0;i<numOfData;i++){
+        for(j=0;j<numOfFeature;j++){
+            for(z=0;z<numOfFeature;z++){
+                covariance[i][j]=data[z][j]*data[i][j];
+            }
+            covariance[i][j]=covariance[i][j]/numOfFeature-average[i]*average[j];
+        }
     }
 }
 
